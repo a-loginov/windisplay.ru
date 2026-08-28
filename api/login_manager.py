eebd4afa3bb1e0ec02a8757e37a85a5e5a57abf7
Solcafffff <a-loginov@school1409.ru>
2026-08-28 09:11:59 +0300

    generate

--- api/login_manager.py
+++ api/login_manager.py
@@ -1,8 +1,11 @@
 import os
+import secrets
+import requests
 from flask_bcrypt import Bcrypt
 from main import app
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_login import login_manager, login_required, current_user
+
 
 
 
@@ -10,3 +13,32 @@ from flask_login import login_manager, login_required, current_user
 @app.route("/")
 def login():
     return render_template("login.html")
+
+
+@app.route("/home")
+@login_required
+def home():
+    return render_template("home.html")
+
+@app.route('/help')
+def help_page():
+    return render_template('help.html')
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
+
+
+@app.route('/create_profile')
+def create_teacher_profile():
+    return render_template('create_profile.html')
+
+# ------------------------------ Backend ------------------------------ #
+
+
+# Тестововый режим #
+PEOPLE_INVATIE_PASSWORD="РОУТЕРСКИЙ"
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import config
+from flask_login import LoginManager
 from flask import Flask, render_template, url_for, session
 from datetime import timedelta
 
@@ -7,6 +8,8 @@ app = Flask(__name__)
 app.config.update(SECRET_KEY=config.SECRET_KEY)
 app.permanent_session_lifetime = timedelta(days=365)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from api.login_manager import *
 #from api.tv import tv
